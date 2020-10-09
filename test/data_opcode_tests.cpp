2c505c9ddaa6565ea5d84b79a8abb6fb33814f1c
Jennifer Suratna <jenyeji@gmail.com>
2020-10-09 15:20:53 -0700

    added unit tests for jmp and xchg

--- test/data_opcode_tests.cpp
+++ test/data_opcode_tests.cpp
@@ -43,3 +43,21 @@ TEST(MOV, RegisterToMem)
 	mov(0x7E, &cpu);
 	EXPECT_EQ(cpu.psw, 0x0100);
 }
+
+TEST(XCHG, All)
+{
+	struct cpu_state cpu
+	{
+		.int_cond = nullptr, .int_lock = nullptr, .memory = nullptr,
+		.interrupt_buffer = nullptr, .data_bus = nullptr,
+		.address_bus = nullptr, .sp = 0, .pc = 0, .bc = 0, .de = 0x1111,
+		.hl = 0x2222, .psw = 0, .halt_flag = 0, .reset_flag = 0,
+		.interrupt_enable_flag = 0
+	};
+
+	// XCHG
+	xchg(0xeb, &cpu);
+	EXPECT_EQ(cpu.de, 0x2222);
+	EXPECT_EQ(cpu.hl, 0x1111);
+	EXPECT_EQ(cpu.pc, 1);
+}
