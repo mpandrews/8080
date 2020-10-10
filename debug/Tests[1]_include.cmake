666e96a67e4cb5a734466ece5ea1aa7e96b84a33
Jordan Bartos <jordankbartos@gmail.com>
2020-10-10 13:38:25 -0500

    remove debug build dir

--- debug/Tests[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/jordan/Documents/schoolwork/CS467/8080/debug/Tests[1]_tests.cmake")
-  include("/home/jordan/Documents/schoolwork/CS467/8080/debug/Tests[1]_tests.cmake")
-else()
-  add_test(Tests_NOT_BUILT Tests_NOT_BUILT)
-endif()
