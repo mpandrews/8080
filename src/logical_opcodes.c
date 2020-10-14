8390ffcd7ad283af61153ed5ca8934f5d8677bdf
Jordan Bartos <jordankbartos@gmail.com>
2020-10-13 21:02:34 -0500

    implemented CMA and CMC plus unit tests

--- src/logical_opcodes.c
+++ src/logical_opcodes.c
@@ -80,14 +80,27 @@ int rar(uint8_t opcode, struct cpu_state* cpu)
 
 int cma(uint8_t opcode, struct cpu_state* cpu)
 {
-	// TODO
-	return placeholder(opcode, cpu);
+	assert(opcode == 0x2f);
+#ifdef VERBOSE
+	fprintf(stderr, "0x%4.4x: CMA\n", cpu->pc);
+#endif
+
+	// XOR the accumulator register to complement each bit
+	cpu->a ^= 0xff;
+	return 1;
 }
 
 int cmc(uint8_t opcode, struct cpu_state* cpu)
 {
-	// TODO
-	return placeholder(opcode, cpu);
+	assert(opcode == 0x3f);
+#ifdef VERBOSE
+	fprintf(stderr, "0x%4.4x: CMC\n", cpu->pc);
+#endif
+
+	// XOR the flags register with the carry flag bit to toggle
+	// just that one bit
+	cpu->flags ^= CARRY_FLAG;
+	return 1;
 }
 
 int stc(uint8_t opcode, struct cpu_state* cpu)
--- test/logical_opcode_tests.cpp
+++ test/logical_opcode_tests.cpp
@@ -6,3 +6,48 @@ extern "C"
 }
 
 #include "gtest/gtest.h"
+
+TEST(CMC, All)
+{
+	struct cpu_state cpu
+	{
+		.int_cond = 0, .int_lock = 0, .memory = nullptr,
+		.interrupt_buffer = 0, .data_bus = 0, .address_bus = 0, .sp = 0,
+		.pc = 0, .bc = 0, .de = 0, .hl = 0, .psw = 0x00ff,
+		.halt_flag = 0, .reset_flag = 0, .interrupt_enable_flag = 0
+	};
+
+	// with all the flags set, call cmc and assert that just the carry flag
+	// was reset and all other flags were unaffected
+	int cycles = cmc(0x3f, &cpu);
+	EXPECT_EQ(cycles, 1);
+	EXPECT_EQ(cpu.flags, (uint8_t) ~CARRY_FLAG);
+
+	// now that the carry flag is reset, call it again and asser that the
+	// flags register is now all set again and all other flags are still set
+	cycles = cmc(0X3f, &cpu);
+	EXPECT_EQ(cycles, 1);
+	EXPECT_EQ(cpu.flags, 0xff);
+}
+
+TEST(CMA, All)
+{
+	struct cpu_state cpu
+	{
+		.int_cond = 0, .int_lock = 0, .memory = nullptr,
+		.interrupt_buffer = 0, .data_bus = 0, .address_bus = 0, .sp = 0,
+		.pc = 0, .bc = 0, .de = 0, .hl = 0, .psw = 0, .halt_flag = 0,
+		.reset_flag = 0, .interrupt_enable_flag = 0
+	};
+
+	// cpu.a is set to 0 to start, assert that it is complemented to 0xff
+	int cycles = cma(0x2f, &cpu);
+	EXPECT_EQ(cycles, 1);
+	EXPECT_EQ(cpu.a, 0xff);
+
+	// now that it is 0xff, complement it again and asser that it has been
+	// reset to 0x00
+	cycles = cma(0X2f, &cpu);
+	EXPECT_EQ(cycles, 1);
+	EXPECT_EQ(cpu.a, 0);
+}
