7b68e91edba1ab454a02fa7cd5cd8434cda29a87
Jennifer Suratna <jenyeji@gmail.com>
2020-11-03 10:51:00 -0800

    clean

--- hardware/si/src/play_sound.cpp
+++ hardware/si/src/play_sound.cpp
@@ -13,49 +13,40 @@ Mix_Chunk** load_sound()
 	// Init array of pointers to SI sound effects
 	Mix_Chunk** sound_effects =
 			(Mix_Chunk**) malloc(8 * sizeof(Mix_Chunk*));
-	int count = 0;
 
-	// sound_effects[0] = invader1_sound mix chunk
 	SDL_RWops* invader1_rw = SDL_RWFromConstMem(invader1, sizeof(invader1));
 	Mix_Chunk* invader1_sound = Mix_LoadWAV_RW(invader1_rw, 1);
-	sound_effects[count++]	  = invader1_sound;
+	sound_effects[0]	  = invader1_sound;
 
-	// sound_effects[1] = invader2_sound mix chunk
 	SDL_RWops* invader2_rw = SDL_RWFromConstMem(invader2, sizeof(invader2));
 	Mix_Chunk* invader2_sound = Mix_LoadWAV_RW(invader2_rw, 1);
-	sound_effects[count++]	  = invader2_sound;
+	sound_effects[1]	  = invader2_sound;
 
-	// sound_effects[2] = invader3_sound mix chunk
 	SDL_RWops* invader3_rw = SDL_RWFromConstMem(invader3, sizeof(invader3));
 	Mix_Chunk* invader3_sound = Mix_LoadWAV_RW(invader3_rw, 1);
-	sound_effects[count++]	  = invader3_sound;
+	sound_effects[2]	  = invader3_sound;
 
-	// sound_effects[3] = invader4_sound mix chunk
 	SDL_RWops* invader4_rw = SDL_RWFromConstMem(invader4, sizeof(invader4));
 	Mix_Chunk* invader4_sound = Mix_LoadWAV_RW(invader4_rw, 1);
-	sound_effects[count++]	  = invader4_sound;
+	sound_effects[3]	  = invader4_sound;
 
-	// sound_effects[4] = invader_killed_sound mix chunk
 	SDL_RWops* invader_killed_rw = SDL_RWFromConstMem(
 			invader_killed, sizeof(invader_killed));
 	Mix_Chunk* invader_killed_sound = Mix_LoadWAV_RW(invader_killed_rw, 1);
-	sound_effects[count++]		= invader_killed_sound;
+	sound_effects[4]		= invader_killed_sound;
 
-	// sound_effects[5] = player_die_sound mix chunk
 	SDL_RWops* player_die_rw =
 			SDL_RWFromConstMem(player_die, sizeof(player_die));
 	Mix_Chunk* player_die_sound = Mix_LoadWAV_RW(player_die_rw, 1);
-	sound_effects[count++]	    = player_die_sound;
+	sound_effects[5]	    = player_die_sound;
 
-	// sound_effects[6] = shot_sound mix chunk
-	SDL_RWops* shot_rw     = SDL_RWFromConstMem(shot, sizeof(shot));
-	Mix_Chunk* shot_sound  = Mix_LoadWAV_RW(shot_rw, 1);
-	sound_effects[count++] = shot_sound;
+	SDL_RWops* shot_rw    = SDL_RWFromConstMem(shot, sizeof(shot));
+	Mix_Chunk* shot_sound = Mix_LoadWAV_RW(shot_rw, 1);
+	sound_effects[6]      = shot_sound;
 
-	// sound_effects[7] = ufo_hit_sound mix chunk
 	SDL_RWops* ufo_hit_rw	 = SDL_RWFromConstMem(ufo_hit, sizeof(ufo_hit));
 	Mix_Chunk* ufo_hit_sound = Mix_LoadWAV_RW(ufo_hit_rw, 1);
-	sound_effects[count]	 = ufo_hit_sound;
+	sound_effects[7]	 = ufo_hit_sound;
 
 	if (!invader1_sound || !invader2_sound || !invader3_sound
 			|| !invader4_sound || !invader_killed_sound
