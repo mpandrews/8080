28699a10c80f899173641ae8b4987803637dcc73
mpandrews <46432743+mpandrews@users.noreply.github.com>
2020-11-05 15:17:32 -0800

    Post video cleanup refactor. (#128)
    * Post video cleanup refactor.
    
    * Removed files accidentally added.
    
    * Fixed off-by-one in cycle_timer.c.
    
    * Removed spurious hw_imports.h; not needed.
    
    * Added Balloon Bomber, Ozma Wars, and Lunar Rescue ROMS.

--- hardware/taito/include/taito_struct.h
+++ hardware/taito/include/taito_struct.h
@@ -6,19 +6,22 @@
 
 struct taito_struct
 {
-	pthread_mutex_t* vbuffer_lock;
-	pthread_cond_t* vbuffer_cond;
-	uint8_t* vbuffer;
-	void* rom_struct;
-	uint8_t* interrupt_buffer;
-	pthread_mutex_t* interrupt_lock;
-	pthread_cond_t* interrupt_cond;
+	pthread_mutex_t* const vbuffer_lock;
+	pthread_cond_t* const vbuffer_cond;
+	uint8_t* const vbuffer;
+	void* const rom_struct;
+	uint8_t* const interrupt_buffer;
+	pthread_mutex_t* const interrupt_lock;
+	pthread_cond_t* const interrupt_cond;
+	pthread_mutex_t* const keystate_lock;
+	pthread_mutex_t* const sound_lock;
+	pthread_mutex_t* const reset_quit_lock;
+	uint8_t* const reset_flag;
+	uint8_t* const quit_flag;
 };
 
 struct taito_struct* create_taito_struct();
 
 void destroy_taito_struct(struct taito_struct*);
-
-void update_vbuffer(uint8_t const*, struct taito_struct*);
 
 #endif
