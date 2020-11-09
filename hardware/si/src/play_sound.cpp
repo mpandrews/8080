d35ede27f071a0cd7d0dc7bb632095ab0d7cd00e
mpandrews <46432743+mpandrews@users.noreply.github.com>
2020-11-08 23:55:44 -0800

    Added new UFO and laser sounds, adjusted how sounds work somewhat.

--- hardware/si/src/play_sound.cpp
+++ hardware/si/src/play_sound.cpp
@@ -5,15 +5,11 @@
 
 Mix_Chunk** load_sound()
 {
-	// Init UFO sound. UFO sound is toggled by hw_output.
-	SDL_RWops* ufo_rw    = SDL_RWFromConstMem(ufo, sizeof(ufo));
-	Mix_Music* ufo_sound = Mix_LoadMUS_RW(ufo_rw, sizeof(ufo));
-	Mix_PlayMusic(ufo_sound, -1);
-
 	// Init array of pointers to SI sound effects
 	Mix_Chunk** sound_effects =
-			(Mix_Chunk**) malloc(8 * sizeof(Mix_Chunk*));
+			(Mix_Chunk**) malloc(9 * sizeof(Mix_Chunk*));
 
+	
 	SDL_RWops* invader1_rw = SDL_RWFromConstMem(invader1, sizeof(invader1));
 	Mix_Chunk* invader1_sound = Mix_LoadWAV_RW(invader1_rw, 1);
 	sound_effects[0]	  = invader1_sound;
@@ -40,7 +36,7 @@ Mix_Chunk** load_sound()
 	Mix_Chunk* player_die_sound = Mix_LoadWAV_RW(player_die_rw, 1);
 	sound_effects[5]	    = player_die_sound;
 
-	SDL_RWops* shot_rw    = SDL_RWFromConstMem(shot, sizeof(shot));
+	SDL_RWops* shot_rw    = SDL_RWFromConstMem(mike_laser, sizeof(mike_laser));
 	Mix_Chunk* shot_sound = Mix_LoadWAV_RW(shot_rw, 1);
 	sound_effects[6]      = shot_sound;
 
@@ -48,6 +44,10 @@ Mix_Chunk** load_sound()
 	Mix_Chunk* ufo_hit_sound = Mix_LoadWAV_RW(ufo_hit_rw, 1);
 	sound_effects[7]	 = ufo_hit_sound;
 
+	SDL_RWops* ufo_rw    = SDL_RWFromConstMem(mike_ufo, sizeof(mike_ufo));
+	Mix_Chunk* ufo_sound = Mix_LoadWAV_RW(ufo_rw, sizeof(mike_ufo));
+	sound_effects[8] = ufo_sound;
+	
 	if (!invader1_sound || !invader2_sound || !invader3_sound
 			|| !invader4_sound || !invader_killed_sound
 			|| !player_die_sound || !shot_sound || !ufo_hit_sound
@@ -62,28 +62,41 @@ void play_sound(void* sounds, void* rom_struct)
 	struct rom_struct* rStruct = (struct rom_struct*) rom_struct;
 	Mix_Chunk** sound_effects  = (Mix_Chunk**) sounds;
 
-	if (rStruct->sound_off) { return; }
-
-	if (rStruct->ufo_sound) { Mix_ResumeMusic(); }
-	else
-	{
-		Mix_PauseMusic();
+	if (rStruct->sound_off)
+       	{ 
+		Mix_HaltChannel(-1);
+       		return;
 	}
+
 
 	if (rStruct->fast_invader1_sound && !Mix_Playing(0))
 	{ Mix_PlayChannel(0, sound_effects[0], 0); }
+	
 	if (rStruct->fast_invader2_sound && !Mix_Playing(1))
 	{ Mix_PlayChannel(1, sound_effects[1], 0); }
+	
 	if (rStruct->fast_invader3_sound && !Mix_Playing(2))
 	{ Mix_PlayChannel(2, sound_effects[2], 0); }
+	
 	if (rStruct->fast_invader4_sound && !Mix_Playing(3))
 	{ Mix_PlayChannel(3, sound_effects[3], 0); }
+	
 	if (rStruct->invader_killed_sound && !Mix_Playing(4))
 	{ Mix_PlayChannel(4, sound_effects[4], 0); }
+	
 	if (rStruct->player_die_sound && !Mix_Playing(5))
 	{ Mix_PlayChannel(5, sound_effects[5], 0); }
+	
 	if (rStruct->shot_sound && !Mix_Playing(6))
-	{ Mix_PlayChannel(6, sound_effects[6], 0); }
+	{ Mix_FadeInChannel(6, sound_effects[6], -1, 50); }
+	else if (!rStruct->shot_sound && Mix_Playing(6))
+	{ Mix_FadeOutChannel(6, 50);}
+
 	if (rStruct->ufo_hit_sound && !Mix_Playing(7))
 	{ Mix_PlayChannel(7, sound_effects[7], 0); }
+	
+	if (rStruct->ufo_sound && !Mix_Playing(8)) 
+	{ Mix_FadeInChannel(8, sound_effects[8], -1, 50); }
+	else if (!rStruct->ufo_sound && Mix_Playing(8))
+	{ Mix_FadeOutChannel(8, 50); }
 }
