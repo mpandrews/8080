e727e476ad93d8c0e4bcd04fc3f136da5a1a8af3
mpandrews <46432743+mpandrews@users.noreply.github.com>
2020-11-12 19:14:39 -0800

    Added custom laser and UFO sounds. (#134)
    * Added new UFO and laser sounds, adjusted how sounds work somewhat.
    
    * Got sane formatting on the sound files.
    
    * Added the new sound files to the style-enforcement exemption.
    
    * clang-format
    
    * Adjusted style-enforcement file.
    
    * Readjusted style-enforcement.
    
    * Style enforcer again.
    
    * Fixed size of laser sound in sound.h.

--- hardware/si/include/sound.h
+++ hardware/si/include/sound.h
@@ -1,16 +1,18 @@
 #ifndef SOUND_H
 #define SOUND_H
 
-#include <stdlib.h>
+#include <stdint.h>
 
-extern const u_int8_t invader1[1026];
-extern const u_int8_t invader2[1086];
-extern const u_int8_t invader3[1098];
-extern const u_int8_t invader4[1142];
-extern const u_int8_t shot[3421];
-extern const u_int8_t player_die[8775];
-extern const u_int8_t invader_killed[4124];
-extern const u_int8_t ufo_hit[1846];
-extern const u_int8_t ufo[51698];
+extern const uint8_t invader1[1026];
+extern const uint8_t invader2[1086];
+extern const uint8_t invader3[1098];
+extern const uint8_t invader4[1142];
+extern const uint8_t shot[3421];
+extern const uint8_t player_die[8775];
+extern const uint8_t invader_killed[4124];
+extern const uint8_t ufo_hit[1846];
+extern const uint8_t ufo[51698];
+extern const uint8_t mike_laser[46350];
+extern const uint8_t mike_ufo[222750];
 
 #endif
