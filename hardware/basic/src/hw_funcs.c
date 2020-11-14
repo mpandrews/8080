cad24f2b9ff1af0f2614f3623fb0225c41ceec54
mpandrews <46432743+mpandrews@users.noreply.github.com>
2020-11-13 18:40:07 -0800

    Semi-working: doubled CR is needed.

--- hardware/basic/src/hw_funcs.c
+++ hardware/basic/src/hw_funcs.c
@@ -5,67 +5,78 @@
 #include <poll.h>
 #include <unistd.h>
 
-extern void cycle_wait(int);
 
-// IN
 int hw_in(const uint8_t* opcode, struct cpu_state* cpu)
 {
 	assert(opcode[0] == 0b11011011);
 	(void) cpu;
 	(void) opcode;
 #ifdef VERBOSE
-	fprintf(stderr, "IN (Hardware: basic)\n");
+	fprintf(stderr, "IN 0x%2.2x (Hardware: basic):\n", opcode[1]);
 #endif
+
 	struct pollfd fds;
 	fds.fd = STDIN_FILENO;
 	fds.events = POLLIN;
+	int is_char_pending = poll(&fds, 1, 0);
+	if (is_char_pending == -1)
+	{
+		perror("Error in polling stdin");
+		*cpu->quit_flag = 1;
+		return 10;
+	}
+	else if (is_char_pending && (fds.revents & (POLLERR | POLLHUP | POLLNVAL)))
+	{
+		*cpu->quit_flag = 1;
+		return 10;
+	}
 
 	switch(opcode[1])
 	{
-		case 0x10: //Status port?
-			cpu->a = 1 | 2 | 4;
+		case 0x10: //Status register
+			
+			cpu->a = 0xff;
+			//if(is_char_pending) cpu->a |= 1;
 			break;
-		case 0x11:
-			switch(poll(&fds, 1, 0))
+		case 0x11: //Input data
+			
+			switch(is_char_pending)
 			{
-				case -1:
-					perror("Error in polling STDIN");
-					*cpu->quit_flag = 1;
-					break;
 				case 0:
 					cpu->a = 0x0;
 					break;
 				default:
-					if (fds.revents & (POLLERR | POLLHUP | POLLNVAL))
 					{
-						*cpu->quit_flag = 1;
+						char input = getchar();
+						if (input == '\n')
+							cpu->a = 0xd;
+						else
+							cpu->a = input;
 						break;
 					}
-					cpu->a = getchar();
-					break;
 			}
 	}
 
-//	printf("IN: %x\n PORT: %x\n", cpu->a, opcode[1]);
 	return 10;
 }
 
-// OUT
 int hw_out(const uint8_t* opcode, struct cpu_state* cpu)
 {
 	assert(opcode[0] == 0b11010011);
 	(void) cpu;
 	(void) opcode;
 #ifdef VERBOSE
-	fprintf(stderr, "OUT (Hardware: basic)\n");
+	fprintf(stderr, "OUT 0x%2.2x (Hardware: basic):\n", opcode[1]);
 #endif
 	switch(opcode[1])
 	{
 		case 0x11:
 			printf("%c", cpu->a);
+			fflush(stdout);
 	}
 	return 10;
 }
+
 
 // Interrupt Hook
 int hw_interrupt_hook(const uint8_t* opcode,
